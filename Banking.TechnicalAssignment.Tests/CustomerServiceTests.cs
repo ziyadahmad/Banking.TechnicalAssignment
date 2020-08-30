e19bda3923cc699875465f85a7058fb4ad9ebf69
Ziyad Ahmad <ziyad.ahmad@mylaps.com>
2020-08-30 17:00:33 +0200

    Added Unit tests

---
+++ Banking.TechnicalAssignment.Tests/CustomerServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Transactions;
+using AutoMapper;
+using Banking.TechnicalAssignment.Api.Core.Domain;
+using Banking.TechnicalAssignment.Api.Core.Dto;
+using Banking.TechnicalAssignment.Api.Core.Respositories;
+using Banking.TechnicalAssignment.Api.Core.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.Features;
+using Moq;
+using Xunit;
+
+namespace Banking.TechnicalAssignment.Tests
+{
+    public class CustomerServiceTests
+    {
+        private readonly Mock<ICustomerRepository> _customerRepository;
+        private readonly Mapper _mapper;
+
+        public CustomerServiceTests()
+        {
+            _customerRepository = new Mock<ICustomerRepository>();
+            _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile(new AutoMapperProfile())));
+        }
+
+        [Fact]
+        public void AddCustomer_WhenCalled_CustomerIsAddedAndNewIdIsReturned()
+        {
+            // arrange
+            int expected = 1;
+            _customerRepository.Setup(x => x.Add(It.IsAny<Customer>())).Returns(expected);
+            _customerRepository.Setup(x => x.NextCustomerId()).Returns(expected);
+
+            // act
+            var service = new CustomerService(_customerRepository.Object, _mapper);
+            var actual = service.AddCustomer(new CustomerDto { Name = "Foo", Surname = "Bar" });
+
+            // assert            
+            Assert.Equal(expected, actual);
+            _customerRepository.Verify(x => x.Add(It.IsAny<Customer>()), Times.Once);
+        }
+
+        [Fact]
+        public void GetCustomerById_WhenCalled_ReturnsCustomerForMatchingId()
+        {
+            // arrange
+            var expected = new Customer { CustomerId = 1, Name = "foo", Surname = "bar" };
+            _customerRepository.Setup(x => x.Get(It.IsAny<Expression<Func<Customer, bool>>>())).Returns(expected);
+
+            // act
+            var service = new CustomerService(_customerRepository.Object, _mapper);
+            var actual = service.GetCustomerById(1);
+
+            // assert            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Banking.TechnicalAssignment.Tests/TransactionServiceTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using AutoMapper;
+using Banking.TechnicalAssignment.Api.Core.Domain;
+using Banking.TechnicalAssignment.Api.Core.Dto;
+using Banking.TechnicalAssignment.Api.Core.Respositories;
+using Banking.TechnicalAssignment.Api.Core.Services;
+using Moq;
+using Xunit;
+
+namespace Banking.TechnicalAssignment.Tests
+{
+    public class TransactionServiceTests
+    {
+        private readonly Mock<IRepository<AccountTransaction>> _transactionRepository;
+        private readonly Mock<IRepository<Account>> _accountRepository;
+
+        private readonly Mapper _mapper;
+
+        public TransactionServiceTests()
+        {
+            _transactionRepository = new Mock<IRepository<AccountTransaction>>();
+            _accountRepository = new Mock<IRepository<Account>>();
+            _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile(new AutoMapperProfile())));
+        }
+
+        [Fact]
+        public void GetTransactions_WhenCalled_ReturnsAllTransactionsForProvidedAccountId()
+        {
+            // arrange
+            var fakeDate = new DateTimeOffset(new DateTime(2020, 08, 30));
+            var transactions = new[]
+            {
+                new AccountTransaction {AccountId =1, Amount=10,Date =fakeDate},
+                new AccountTransaction {AccountId =2, Amount=15,Date =fakeDate}
+            };
+
+            _transactionRepository.Setup(x => x.GetAllById(It.IsAny<Expression<Func<AccountTransaction, bool>>>())).Returns(transactions);
+
+            // act
+            var service = new TransactionService(_transactionRepository.Object, _accountRepository.Object);
+            var actual = service.GetTransactions(1);
+
+            // assert            
+            Assert.Equal(transactions, actual);
+            _transactionRepository.Verify(x => x.GetAllById(It.IsAny<Expression<Func<AccountTransaction, bool>>>()), Times.Once);
+        }
+
+        [Fact]
+        public void InsertTransactions_WhenCalled_AddTransactionsAndUpdateAccountBalance()
+        {
+            // arrange
+            var fakeDate = new DateTimeOffset(new DateTime(2020, 08, 30));
+            var account = new Account { AccountId = 1, Balance = 10 };
+            var accountTransaction = new AccountTransaction { AccountId = 1, Amount = 10, Date = fakeDate };
+
+            _accountRepository.Setup(x => x.Get(It.IsAny<Expression<Func<Account, bool>>>())).Returns(account);
+            _accountRepository.Setup(x => x.Update(It.IsAny<Account>())).Returns(true);
+            _transactionRepository.Setup(x => x.Add(It.IsAny<AccountTransaction>())).Returns(1);
+
+            // act
+            var service = new TransactionService(_transactionRepository.Object, _accountRepository.Object);
+            service.InsertTransaction(accountTransaction);
+            
+            // assert                        
+            _transactionRepository.Verify(x => x.Add(It.IsAny<AccountTransaction>()), Times.Once);
+            _accountRepository.Verify(x => x.Update(It.IsAny<Account>()), Times.Once);
+        }
+    }
+}
